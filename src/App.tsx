2d424fed35cd39adedef0122b0773ddece024f64
Mapie Mamunhe <mmamunhe@gmail.com>
2022-10-23 15:35:13 +0200

    Update react-query client global defaults

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,22 @@ import CardSelection from "./components/CardSelection";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Timer from "./components/Timer";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchInterval: false,
+      staleTime: Infinity,
+    },
+  },
+});
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <Timer />
       <CardSelection />
@@ -15,7 +28,8 @@ function App() {
         </button>
       </div>
       <Footer />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 

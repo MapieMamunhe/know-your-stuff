7e38d7fa20c744cff19b8f4df291f5feef215969
Mapie Mamunhe <mmamunhe@gmail.com>
2022-12-14 13:55:47 +0200

    Update timer logic

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,13 +1,19 @@
+import { useAtom } from "jotai";
 import React, { useState } from "react";
-
+import { gameOverAtom } from "../Button";
 // import { Container } from './styles';
 
 const Timer: React.FC = () => {
-  const [timer, setTimer] = useState(60);
+  const [timer, setTimer] = useState(10);
+  const [gameOver, setGameOver] = useAtom(gameOverAtom);
   const timerFunction = setTimeout(() => {
-    setTimer(() => (timer <= 0 ? 60 : timer - 1));
+    setTimer(() => timer - 1);
     return () => clearTimeout(timerFunction);
   }, 1000);
+  if (timer <= 0) {
+    clearTimeout(timerFunction);
+    setGameOver(true);
+  }
   return <p className="text-center font-semibold text-2xl">{timer}</p>;
 };
 
